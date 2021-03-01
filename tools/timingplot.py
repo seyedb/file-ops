896cd2baa18bf2af60e3abc71704330f18730d3b
seyedb <m.h.banihashemian@gmail.com>
2021-03-01 00:34:57 -0500

    add tools directory and timing plot script

---
+++ tools/timingplot.py
@@ -0,0 +1,66 @@
+
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+df = pd.read_table('../data/getline.txt', names=['line number', 'time'])
+df_binsrch = pd.read_table('../data/getline_binsrch.txt', names=['line number', 'time'])
+
+df = df.sort_values(by=['line number'])
+df_binsrch = df_binsrch.sort_values(by=['line number'])
+
+lin_nums = df.iloc[:,0]
+
+trace1 = {'x': lin_nums, 
+          'y': df.iloc[:,1],
+          'type': 'scatter',
+          'name': 'getLine',
+          'mode': 'markers',
+          'type': 'scatter',
+          'showlegend': True}
+
+trace2 = {'x': lin_nums, 
+          'y': df_binsrch.iloc[:,1],
+          'type': 'scatter',
+          'name': 'getLine_binarysearch',
+          'mode': 'markers',
+          'type': 'scatter',
+          'showlegend': True}
+
+trend = px.scatter(df, x='line number', y='time', trendline='lowess')
+nonlinfit = trend.data[1].y
+
+trend = px.scatter(df_binsrch, x='line number', y='time', trendline='lowess')
+nonlinfit_binsrch = trend.data[1].y
+
+trace1fit = {'x': lin_nums, 
+          'y': nonlinfit,
+          'type': 'scatter',
+          'name': 'getLine LOWESS fit',
+          'mode': 'lines',
+          'type': 'scatter',
+          'showlegend': True}
+
+trace2fit = {'x': lin_nums, 
+          'y': nonlinfit_binsrch,
+          'type': 'scatter',
+          'name': 'getLine_binarysreach LOWESS fit',
+          'mode': 'lines',
+          'type': 'scatter',
+          'showlegend': True}
+
+layout = go.Layout({
+          'xaxis': {'title': 'line number', 'rangemode': 'nonnegative'},
+          'yaxis': {'title': 'time (s)', 'rangemode': 'nonnegative'}
+         })
+
+fig = go.Figure()
+
+fig.add_trace(trace1)
+fig.add_trace(trace2)
+fig.add_trace(trace1fit)
+fig.add_trace(trace2fit)
+fig.update_layout(layout)
+
+fig.show()
+
