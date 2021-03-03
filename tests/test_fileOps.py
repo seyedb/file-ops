6071c30af5f79d97ee10fe19fd249bda786f221e
seyedb <m.h.banihashemian@gmail.com>
2021-03-03 00:06:03 -0500

    add tests

--- tests/test_fileOps.py
+++ tests/test_fileOps.py
@@ -1,6 +1,7 @@
 # these tests are desigend for pytest framework
 
 import fileOps as fo
+import numpy as np
 
 def test_getLine():
     refline = "JAQUES\tAll the world's a stage,\n"
@@ -13,3 +14,30 @@ def test_getLine():
     result = line.partition("\t")[2].rstrip()
     assert result == reference
 
+def test_addLineNumber():
+    fin = "../data/shakespeare.txt"
+    fin_wln = "../data/shakespeare-wln.txt"
+    numlines = 4122 # total number of lines
+
+    fo.addLineNumber(fin, fin_wln)
+
+    rng = np.random.default_rng()
+    lnumber = rng.integers(low=1, high=numlines)
+
+    line = fo.getLine(fin_wln, lnumber)
+    ln = int(line.split()[0])
+    assert ln == lnumber
+
+def test_addLineNumber_inplace():
+    fin = "../data/shakespeare.txt"
+    fin_wln_inplace = "../data/shakespeare-wln-inplace.txt"
+    numlines = 4122
+
+    fo.addLineNumber_inplace(fin_wln_inplace)
+
+    rng = np.random.default_rng()
+    lnumber = rng.integers(low=1, high=numlines)
+
+    line = fo.getLine(fin_wln_inplace, lnumber)
+    ln = int(line.split()[0])
+    assert ln == lnumber
