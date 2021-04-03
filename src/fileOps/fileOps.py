637aa3ef98665110a5c0afb45c39cae49e1ad7b1
seyedb <m.h.banihashemian@gmail.com>
2021-04-02 20:24:48 -0400

    refactor

--- src/fileOps/fileOps.py
+++ src/fileOps/fileOps.py
@@ -1,16 +1,12 @@
-#
 import os
 import sys
 import fileinput
-import numpy as np
 
 import re
 from re import findall
 
 import json
 from json import loads
-
-import timeit
 
 def getLine(fname, lnumber):
     """Jumps to a line number in a file and reads a line.
@@ -183,53 +179,3 @@ class loaded_json(object):
     def __init__(self, json_file):
         with open(json_file, 'r') as fid:
             self.__dict__ = json.loads(fid.read())
-
-def benchmark_getLine(fin, numlines, path):
-    """Compare execution time of getLine functions.
-
-    Args:
-        fin (str): path to the input file.
-        numlines (int): the total number of line of fin.
-        path (str): where to store timing results.
-    """
-    fname, fext = os.path.splitext(fin)
-
-    # fin with line number will be generated in the same path as fin
-    fin_wln = '{}{}{}'.format(fname,'_wln',fext)
-    fin_wln_inplace = '{}{}{}'.format(fname,'-inplace',fext)
-
-    # add line numbers, compare two functions wrt timing 
-    starttime = timeit.default_timer()
-    addLineNumber(fin, fin_wln)
-    print("addLineNumber :", timeit.default_timer() - starttime)
-
-    starttime = timeit.default_timer()
-    addLineNumber_inplace(fin_wln_inplace)
-    print("addLineNumber_inplace  :", timeit.default_timer() - starttime)
-
-    # generate sz distinct random integers between 1 and numlines as line numbers to be read
-    sz = 1000 
-    rng = np.random.default_rng()
-    lnumbers = rng.choice(numlines, size=sz, replace=False)
-    with open('../data/lnumbers.txt', 'w') as flnums:
-        print('\n'.join(map(str, lnumbers)), file=flnums)
-
-    # timing for getLine function
-    getline_time=[]
-    for ln in lnumbers:
-        starttime = timeit.default_timer()
-        line = getLine(fin, ln)
-        getline_time.append(timeit.default_timer() - starttime)
-
-    with open(path + '/getline_time.txt', 'w') as fgetlinetime:
-        print('\n'.join(map(str, getline_time)), file=fgetlinetime)
-
-    # timing for getLine_binarysearch function
-    getline_bs_time=[]
-    for ln in lnumbers:
-        starttime = timeit.default_timer()
-        line = getLine_binarysearch(fin_wln, ln)
-        getline_bs_time.append(timeit.default_timer() - starttime)
-
-    with open(path + '/getline_binsrch_time.txt', 'w') as fgetlinebstime:
-        print('\n'.join(map(str, getline_bs_time)), file=fgetlinebstime)
